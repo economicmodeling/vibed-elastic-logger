c96120dce45b309fd43f480e1a70130c4855747c
Hackerpilot <briancschott@gmail.com>
2018-02-06 16:27:38 -0800

    Fill out the rest of the fields on the log message

--- source/vibe_elastic_logger/logger.d
+++ source/vibe_elastic_logger/logger.d
@@ -12,6 +12,24 @@ private alias MessageBuffer = typeof(appender!(char[])());
 // Bypass to!string for log levels because we look them up frequently
 private immutable string[LogLevel.max + 1] levelToString;
 
+private string escapeChars(char ch) pure nothrow @safe
+{
+    switch (ch)
+    {
+    case '"':
+        return `\"`;
+    default:
+        return () @trusted { return cast(string)[ch]; }();
+    }
+}
+
+private void putEscapedString(ref typeof(appender!string()) buffer, const(char)[] str) @safe
+{
+    import std.utf : byCodeUnit;
+
+    str.byCodeUnit().map!(ch => escapeChars(ch)).each!(s => buffer.put(s));
+}
+
 shared static this()
 {
     levelToString[LogLevel.trace] = "trace";
@@ -33,36 +51,64 @@ shared static this()
 alias IndexCreator = string delegate() @safe;
 
 /**
+ * ElasticSearch server connection information.
+ */
+struct ElasticInfo
+{
+    /// Host name for the ElasticSearch server.
+    string hostName;
+    // Port for connecting to the ElasticSearch server.
+    ushort portNumber = 9200;
+    // For the "type" that the log messages will be given within an index.
+    string typeName;
+}
+
+/**
+ * ElasticSearch logger implementation.
  *
+ * Messages are queued and written to the server when any of the following
+ * conditions are met:
+ * $(UL
+ * $(LI The message queue is full.)
+ * $(LI The message to be logged has a LogLevel of `critical` or higher.)
+ * $(LI A message is queued to be logged, and more time than the `maxLogInterval`
+ *     constructor argument has passed.)
+ * $(LI The logger is asked to log the message "Main thread exiting".)
+ * )
+ *
+ * The name of the index that the logger writes messages to is determined by a
+ * function (the `indexCreator` constructor argument). This usually returns an
+ * index name based off of the current time.
  */
 class ElasticLogger : Logger
 {
     /**
      * Params:
+     *     elasticInfo = Connection information to use when logging messages.
      *     indexCreator = A function that will return the name of the index to
      *         log to.
      *     maxLogInterval = If a log message is received and more than this
      *         amount of time has passed since the last flush, trigger a flush
      *         regardless of the remaining space in the buffer.
-     *     bufferSize = Number of log messages to queue between writes to
+     *     messageQueueSize = Number of log messages to queue between writes to
      *         ElasticSearch.
      */
-    this(const string elasticUrl, const IndexCreator indexCreator,
-            const Duration maxLogInterval, const size_t bufferSize)
+    this(const ElasticInfo elasticInfo, const IndexCreator indexCreator,
+            const Duration maxLogInterval, const size_t messageQueueSize)
     {
         this.indexCreator = indexCreator;
         this.maxLogInterval = maxLogInterval;
-        this.entries = new LogEntry[](bufferSize);
+        this.entries = new LogEntry[](messageQueueSize);
         this.multilineLogger = true;
         this.minLevel = LogLevel.trace;
         this.lastFlushTime = Clock.currTime();
-        this.elasticUrl = elasticUrl;
-        this.logBufferIndex = 0;
+        this.elasticInfo = elasticInfo;
+        this.logQueueIndex = 0;
     }
 
     override void beginLine(ref LogLine line) @safe
     {
-        LogEntry* l = &entries[logBufferIndex];
+        LogEntry* l = &entries[logQueueIndex];
         l.module_ = line.mod;
         l.function_ = line.func;
         l.file = line.file;
@@ -76,17 +122,18 @@ class ElasticLogger : Logger
 
     override void endLine() @safe
     {
-        immutable r = logBufferIndex;
-        logBufferIndex++;
-        if (logBufferIndex == entries.length || entries[r].lLevel >= LogLevel.critical
-                || entries[r].buffer.data == "Main thread exiting"
+        immutable r = logQueueIndex;
+        logQueueIndex++;
+        if (logQueueIndex == entries.length || entries[r].lLevel >= LogLevel.critical
+                || (entries[r].lLevel == LogLevel.diagnostic
+                    && entries[r].buffer.data == "Main thread exiting")
                 || lastFlushTime + maxLogInterval < Clock.currTime())
             flush();
     }
 
     override void put(scope const(char)[] text) @safe
     {
-        entries[logBufferIndex].buffer.put(text);
+        entries[logQueueIndex].buffer.put(text);
     }
 
 private:
@@ -106,29 +153,45 @@ private:
 
     void flush() @safe
     {
-        import std.utf : byCodeUnit;
+        import std.conv : to;
+        import std.format : format;
 
         immutable elasticIndex = indexCreator();
-        immutable url = elasticUrl ~ elasticIndex ~ "/messages/_bulk";
-        () @trusted{ stderr.writeln("\033[01;33m", url, "\033[0m"); }();
+        immutable url = format!"http://%s:%d/%s/%s/_bulk"(elasticInfo.hostName,
+                elasticInfo.portNumber, elasticIndex, elasticInfo.typeName);
+        version(debug_elastic_logger)
+        {
+            () @trusted { stderr.writeln("\033[01;33m", url, "\033[0m"); }();
+        }
         auto requestBody = appender!string();
-        foreach (ref entry; entries[0 .. logBufferIndex])
+        foreach (ref entry; entries[0 .. logQueueIndex])
         {
             requestBody.put(`{"index": {}}`);
             requestBody.put("\n");
-            requestBody.put(`{"message": "`);
-            entry.buffer.data.byCodeUnit().map!((char ch) @safe {
-                    switch (ch) {
-                        case '"': return `\"`;
-                        default: return () @trusted { return cast(string) [ch]; }();
-                    }
-                })
-                .each!(s => requestBody.put(s));
+            requestBody.put(`{"message":"`);
+            requestBody.putEscapedString(entry.buffer.data);
+            requestBody.put(`","module":"`);
+            requestBody.put(entry.module_);
+            requestBody.put(`","function":"`);
+            requestBody.putEscapedString(entry.function_);
+            requestBody.put(`","file":"`);
+            requestBody.putEscapedString(entry.file);
+            requestBody.put(`","line":"`);
+            requestBody.put(entry.line.to!string());
+            requestBody.put(`","level":"`);
+            requestBody.put(entry.level);
+            requestBody.put(`","fiberID":"`);
+            requestBody.put(entry.fiberID.to!string());
+            requestBody.put(`","time":"`);
+            requestBody.put(entry.time);
             requestBody.put(`"}`);
             requestBody.put("\n");
         }
-        () @trusted{ stderr.writeln("\033[01;33m", requestBody.data, "\033[0m"); }();
-        logBufferIndex = 0;
+        version(debug_elastic_logger)
+        {
+            () @trusted { stderr.writeln("\033[01;33m", requestBody.data, "\033[0m"); }();
+        }
+        logQueueIndex = 0;
         this.lastFlushTime = Clock.currTime();
     }
 
@@ -136,6 +199,6 @@ private:
     const IndexCreator indexCreator;
     const Duration maxLogInterval;
     SysTime lastFlushTime;
-    const string elasticUrl;
-    size_t logBufferIndex;
+    const ElasticInfo elasticInfo;
+    size_t logQueueIndex;
 }
